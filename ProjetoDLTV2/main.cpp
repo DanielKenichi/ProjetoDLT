8195974fb397d829c83fdd83c34357e1ddfba13a
LuckyLucario <danielkttateishi@gmail.com>
2021-05-27 17:50:09 -0300

    Commit Inicial

---
+++ ProjetoDLTV2/main.cpp
@@ -0,0 +1,32 @@
+//includes e bibliotecas
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
